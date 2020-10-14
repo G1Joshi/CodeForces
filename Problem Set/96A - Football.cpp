008349b9aee5512215ff4194136c8f0153f2032d
Jeevan Chandra Joshi <52780516+G1Joshi@users.noreply.github.com>
2020-10-14 15:47:01 +0530

    Create 96A - Football.cpp

---
+++ Problem Set/96A - Football.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    int c=1, max=-1;
+    for(int i=1; i<str.length(); i++) {
+        if(str[i]==str[i-1]) {
+            ++c;
+            max = max>=c ? max : c;
+        }
+        else
+            c=1;
+    }
+    if(max>=7)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+}
