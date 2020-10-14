1782ba54da7fc36e5beca1b1dcaa07e28dad150b
Jeevan Chandra Joshi <52780516+G1Joshi@users.noreply.github.com>
2020-10-14 16:33:09 +0530

    Create 546A - Soldier and Bananas.cpp

---
+++ Problem Set/546A - Soldier and Bananas.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, n, w, t=0;
+    cin>>k>>n>>w;
+    for(int i=1; i<=w; i++)
+    {
+        t+=i*k;
+    }
+    if(t>n)
+        cout<<t-n;
+    else
+        cout<<"0";
+    return 0;
+}
