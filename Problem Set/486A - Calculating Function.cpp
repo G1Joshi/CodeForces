2bcb30dfc3ba55f242ef543b3ae20ee790c2d15a
Jeevan Chandra Joshi <52780516+G1Joshi@users.noreply.github.com>
2020-10-16 19:26:12 +0530

    Create 486A - Calculating Function.cpp

---
+++ Problem Set/486A - Calculating Function.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    ll p=n/2, q=(n+1)/2;
+    cout<<(p*p+p)-(q*q);
+    return 0;
+}
