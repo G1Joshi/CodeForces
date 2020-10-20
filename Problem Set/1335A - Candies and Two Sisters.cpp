522917f3a6744ac7278a5d43e716e2e1ab848d75
Jeevan Chandra Joshi <52780516+G1Joshi@users.noreply.github.com>
2020-10-20 07:43:16 +0530

    Create 1335A - Candies and Two Sisters.cpp

---
+++ Problem Set/1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n;
+        cin>>n;
+        cout<<(n-1)/2<<"\n";
+    }
+    return 0;
+}
